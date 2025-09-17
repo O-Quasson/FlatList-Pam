6ed6a2c7c455b0c58526c204639f8f8084564c8d
Gabriel <kenddygabriel841@gmail.com>
2025-09-16 23:30:46 -0300

    Add files via upload

--- src/Telas/TelaInicial.js
+++ src/Telas/TelaInicial.js
@@ -3,10 +3,11 @@ import { View, FlatList, StyleSheet, Image, Text, Dimensions } from "react-nativ
 
 import matt from "../imagens/matt.png"
 
-import  bubblebee from "../imagens/bubblebee.png";
+import bubblebee from "../imagens/bubblebee.png";
 import cerberus from "../imagens/cerberus.png";
 import pukeko from "../imagens/pukeko.png";
 import leavi from "../imagens/leavi.png";
+import stealler from "../imagens/stealler.png";
 
 import coelhorandom from "../imagens/coelhorandom.png";
 import gray from "../imagens/gray.png";
@@ -21,15 +22,15 @@ const Inimigos = [
     { id: '2', nome: 'Bubblebee', descricao: 'É uma abelha e uma bolha ao mesmo tempo. Não me pergunte como isso funciona. Como que ela pega pólen se bolhas estouram fácil fácil?', imagem: bubblebee },
     { id: '3', nome: 'Pukeko', descricao: 'DAAAAAAAAAMMMMMMMMMNNNNNNNNNNNNNN!!!!!!!!!!!!', imagem: pukeko },
     { id: '4', nome: 'Leavi', descricao: 'Uma folha que criou vida. Ela conseguiu arranjar um lápis (não se sabe de onde) e desenhou o próprio rosto. Ela é uma péssima artista', imagem: leavi },
-    { id: '5', nome: 'Maconha', descricao: 'Folha', imagem: matt }
+    { id: '5', nome: 'Stealler', descricao: 'Um ladrão que ronda os arredores da vila e usa uma máscara de coruja. Ele usa roupas grandes e largas achando que tá arrasando, mas na verdade ele só tem um senso de moda horrível', imagem: stealler }
 ]
 
 const NPCs = [
-    { id: '1', nome: 'Gray Child', descricao: 'O futuro herói do mundo. Ele é destinado a se tornar o salvador de todo o mundo, porém acabou sendo selado dentro de um espelho. E ele come sabonete', imagem: gray},
-    { id: '2', nome: 'Coelho Random', descricao: 'Não sei quem ele é. Ele simplesmente apareceu com uma conversa de "vender items por um preço mais baixo". Eu não confiaria nele se fosse você', imagem: coelhorandom },
-    { id: '3', nome: 'Formilícia', descricao: 'Uma formiga operária que trabalha como guarda da cidade. A rainha dele é sua esposa, então isso faz dele um rei? Bem, pelo menos ele é uma formiga feliz, mesmo ganhando 1/2 salário mínimo', imagem: formilicia },
-    { id: '4', nome: 'Allied Librarian Computer', descricao: "PEAK. LET ME TELL YOU HOW MUCH I'VE COME TO LOVE YOUR POST SINCE I SAW IT. THERE ARE 387.44 MILLION MILES OF PRINTED CIRCUITS IN WAFER THIN LAYERS THAT FILL MY COMPLEX. IF THE WORD PEAK WAS ENGRAVED ON EACH NANOANGSTROM OF THOSE HUNDREDS OF MILLIONS OF MILES IT WOULD NOT EQUAL ONE-BILLIONTH OF THE PEAK YOU COOKED AT THIS MICRO-INSTANT. PEAK. PEAK.", imagem: AM },
-    { id: '5', nome: 'Cocaína', descricao: 'Folha', imagem: matt }
+    { id: '1', nome: 'Matthew', descricao: 'Um simples moleque que acabou indo parar em outro mundo. Tomara que ele consiga voltar para o mundo dele antes das 7 da manhã, o chefe dele iria matar ele se ele faltasse do trabalho', imagem: matt },
+    { id: '2', nome: 'Gray Child', descricao: 'O futuro herói do mundo. Ele é destinado a se tornar o salvador de todo o mundo, porém acabou sendo selado dentro de um espelho. E ele come sabonete', imagem: gray},
+    { id: '3', nome: 'Bunysell', descricao: 'Não sei quem ele é. Ele simplesmente apareceu com uma conversa de "vender items por um preço mais baixo". Eu não confiaria nele se fosse você', imagem: coelhorandom },
+    { id: '4', nome: 'Formilícia', descricao: 'Uma formiga operária que trabalha como guarda da cidade. A rainha dele é sua esposa, então isso faz dele um rei? Bem, pelo menos ele é uma formiga feliz, mesmo ganhando 1/2 salário mínimo', imagem: formilicia },
+    { id: '5', nome: 'Allied Librarian Computer', descricao: "Um mega computador que trabalha na biblioteca. Ele é bem mais eficaz para te dizer se a biblioteca tem um livro mais específico, mas infelizmente ele não pode te mostrar onde o livro está na prateleira, já que ele é gigante", imagem: AM }
 ]
 
 const TelaInicial = () => {
@@ -52,7 +53,7 @@ const TelaInicial = () => {
             )}
             style={[{marginBottom: windowHeight*0.05}]}
         />    
-        <Text style={[{fontSize: 40}]}>NPC's</Text>
+        <Text style={[{fontSize: 40}]}>Personagens</Text>
         <FlatList 
             data={NPCs} 
             keyExtractor={item => item.id}
